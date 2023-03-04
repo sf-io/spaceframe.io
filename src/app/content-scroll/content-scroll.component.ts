687017e0149eedb211110c908d7bb1db2b4433f1
polychrom <poly.chrom@outlook.com>
2023-03-04 22:08:33 +0100

    removed unused code

--- src/app/content-scroll/content-scroll.component.ts
+++ src/app/content-scroll/content-scroll.component.ts
@@ -1,16 +1,6 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  OnInit,
-} from '@angular/core';
-import { distinctUntilChanged, fromEvent } from 'rxjs';
-import {
-  BOTH_SIDES_MATCH,
-  NEXT_SLIDE,
-  PREV_SLIDE,
-  PROJECT_IN_VIEW,
-} from '../events';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { PROJECT_IN_VIEW } from '../events';
 import { data } from '../data';
 
 @Component({
@@ -24,34 +14,16 @@ export class ContentScrollComponent implements OnInit, AfterViewInit {
 
   public isMatch = false;
 
-  private readonly matchThreshold = 200;
-
-  constructor(private $cdr: ChangeDetectorRef) {}
+  constructor() {}
 
   ngOnInit(): void {
     PROJECT_IN_VIEW.subscribe((id) => (this.currentId = id));
-
-    BOTH_SIDES_MATCH.pipe(distinctUntilChanged()).subscribe((state) =>
-      console.log('both match?', state)
-    );
   }
 
   ngAfterViewInit(): void {
     const projects = Array.from(document.querySelectorAll('.project') || []);
 
-    projects.forEach((element) =>
-      console.log('height', element.getBoundingClientRect().height)
-    );
-
     fromEvent(window, 'scroll').subscribe(() => {
-      const inViewport = !!projects.filter(
-        (project) =>
-          project.getBoundingClientRect().top < window.innerHeight &&
-          project.getBoundingClientRect().top > 200
-      ).length;
-
-      BOTH_SIDES_MATCH.next(inViewport);
-
       for (let projectId = 0; projectId < projects.length; projectId++) {
         let yPosition = projects[projectId].getBoundingClientRect().top;
 
