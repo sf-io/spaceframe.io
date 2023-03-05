ce6a137f0a6708b1c0e55a02ecaff5ef849218c9
polychrom <poly.chrom@outlook.com>
2023-03-05 11:45:00 +0100

    removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { StackComponent } from './stack/stack.component';
-import { LogoComponent } from './logo/logo.component';
 import { SliderComponent } from './slider/slider.component';
 import { IntroComponent } from './intro/intro.component';
 import { FilenameToImageAltPipe } from './filename-to-image-alt.pipe';
-import { NavigationItemsComponent } from './navigation-items/navigation-items.component';
-import { ModalNavigationComponent } from './modal-navigation/modal-navigation.component';
 import { ContentScrollComponent } from './content-scroll/content-scroll.component';
 import { ContentFixedComponent } from './content-fixed/content-fixed.component';
 import { ContentScrollLeftComponent } from './content-scroll-left/content-scroll-left.component';
@@ -20,12 +17,9 @@ import { ContentScrollLeftComponent } from './content-scroll-left/content-scroll
     AppComponent,
     NavigationComponent,
     StackComponent,
-    LogoComponent,
     SliderComponent,
     IntroComponent,
     FilenameToImageAltPipe,
-    NavigationItemsComponent,
-    ModalNavigationComponent,
     ContentScrollComponent,
     ContentFixedComponent,
     ContentScrollLeftComponent,
