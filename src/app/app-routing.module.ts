b9eb952c5994550b62228a9101ac157b1144f9c9
polychrom <poly.chrom@outlook.com>
2023-04-26 17:48:51 +0200

    removed v1 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CommitsComponent } from './commits/commits.component';
-import { HomeComponent } from './home/home.component';
-import { StackComponent } from './stack/stack.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CommitsComponent} from './commits/commits.component';
+import {StackComponent} from './stack/stack.component';
 
 const routes: Routes = [
-  { path: 'commits', component: CommitsComponent },
-  { path: '', component: HomeComponent },
-  { path: 'stack', component: StackComponent },
+  {path: 'commits', component: CommitsComponent},
+  {path: 'stack', component: StackComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
