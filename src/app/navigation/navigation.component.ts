dd187b3e27f8f463af0edbdb600272f881b9f32d
polychrom <poly.chrom@outlook.com>
2023-04-26 22:33:55 +0200

    initial layout

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  projects = ['Project1', 'Project2', 'Project3'];
+
+  stack = ['Angular', 'Vue', 'Typescript', 'CSS'];
+
+  areas = ['Frontend', 'User Interface', 'User Experience', '3d', 'Rendering'];
+
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
