1afea9c7784af2b82ae56acb8419d77d6b841cff
polychrom <poly.chrom@outlook.com>
2023-04-30 18:51:35 +0200

    added typeeffect

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -12,7 +12,16 @@ export class NavigationComponent implements OnInit {
   expertise = ['Frontend Engineering', 'Page Performance', 'User Interfaces', 'User Experience', '3d Rendering'];
 
   commits: any = [];
-  
+
+  readonly logoTitle = 'spaceframe';
+  logoEffect = '';
+
+  counter = 0;
+  intervalID: any;
+  charArr: string[] = [];
+//ABCDEFGHIJKLMNOPQRSTUVWXYZ
+  digits = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
+
   constructor(private $apiService: ApiService) {
     this.$apiService.getData().subscribe(response => {
       if (response.length > 0) {
@@ -27,6 +36,27 @@ export class NavigationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.typeEffect();
   }
 
+  typeEffect(): void {
+    this.counter = 0;
+    this.intervalID = setInterval(() => this._typeEffect(), 100);
+  }
+
+  private _typeEffect(): void {
+    this.charArr = this.logoTitle.split("");
+
+    for (let i = this.counter; i < this.charArr.length; i++) {
+      const pos = Math.floor(Math.random() * this.digits.length - 1);
+      this.charArr[i] = this.digits[pos];
+    }
+
+    this.logoEffect = this.charArr.join('');
+
+    if (this.counter >= this.charArr.length) {
+      clearInterval(this.intervalID);
+    }
+    this.counter++;
+  }
 }
