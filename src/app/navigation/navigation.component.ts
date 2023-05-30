7e0b7b1c7adfd26f48803c6471021772257901fd
polychrom <poly.chrom@outlook.com>
2023-05-30 22:41:11 +0200

    updated text

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,31 +1,50 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiService} from "../api.service";
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
-  stack = ['Angular × Universal', 'Vue × Nuxt', 'TypeScript', 'JavaScript', '(S)CSS', 'Figma', 'Adobe Cloud', 'Cinema4D'];
+  stack = [
+    'Angular × Universal',
+    'Vue × Nuxt',
+    'TypeScript',
+    'JavaScript',
+    '(S)CSS',
+    'Figma',
+    'Adobe Cloud',
+    'Cinema4D',
+  ];
 
-  expertise = ['Frontend Engineering', 'Page Performance', 'User Interfaces', 'User Experience', '3d Rendering'];
+  expertise = [
+    'Frontend Engineering',
+    'Single Page Applications',
+    'Server Side Rendering',
+    'Performance Optimization',
+    'User Interfaces',
+    'User Experience',
+    'Graphic Design',
+    '3d Rendering',
+  ];
 
   commits: any = [];
 
   readonly logoTitle = 'spaceframe';
   logoEffect = '';
 
-  intro = 'Digital playground of spaceframe, a frontend engineer & creative technologist from Germany, who builds digital product experiences across web apps, interfaces and 3D.'
+  intro =
+    'Digital playground of spaceframe, a frontend engineer & creative technologist from Germany, who builds digital product experiences across web apps, interfaces and 3D.';
 
   counter = 0;
   intervalID: any;
   charArr: string[] = [];
-//ABCDEFGHIJKLMNOPQRSTUVWXYZ
-  digits = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
+  chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+  digits = this.chars.split('');
 
   constructor(private $apiService: ApiService) {
-    this.$apiService.getData().subscribe(response => {
+    this.$apiService.getData().subscribe((response) => {
       if (response.length > 0) {
         response.forEach((item: any) => {
           let commit: any = {};
@@ -34,7 +53,7 @@ export class NavigationComponent implements OnInit {
           this.commits.push(commit);
         });
       }
-    })
+    });
   }
 
   ngOnInit(): void {
@@ -47,7 +66,7 @@ export class NavigationComponent implements OnInit {
   }
 
   private _typeEffect(): void {
-    this.charArr = this.logoTitle.split("");
+    this.charArr = this.logoTitle.split('');
 
     for (let i = this.counter; i < this.charArr.length; i++) {
       const pos = Math.floor(Math.random() * this.digits.length - 1);
