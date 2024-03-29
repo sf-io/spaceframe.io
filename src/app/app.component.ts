b73bbd92aa911bb2793bd5355d857adc5ce1c912
polychrom <poly.chrom@outlook.com>
2024-03-29 11:40:06 +0100

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import {
   Component,
-  ComponentFactoryResolver,
-  ViewContainerRef,
 } from '@angular/core';
-import { HelperService } from './helper.service';
 
 @Component({
   selector: 'app-root',
@@ -11,43 +8,7 @@ import { HelperService } from './helper.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'spaceframe.io';
 
-  readonly projects = [
-    {
-      title: 'Street-One/Cecil',
-      text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, seddiam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      imageFirst: 30, //TODO use rows 1-12?
-      images: [{}],
-      stack: ['Angular', 'Docker', 'TypeScript', 'SCSS'],
-      client: 'Personal Project',
-      year: 2023,
-      type: 'Web App',
-    },
-    {
-      title: 'ScrumPoker App',
-      text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, seddiam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      stack: ['Angular', 'Docker', 'TypeScript', 'SCSS'],
-      client: 'Personal Project',
-      year: 2023,
-    },
-    {
-      title: 'Weather App',
-      text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, seddiam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-      stack: ['Angular', 'Docker', 'TypeScript', 'SCSS'],
-      client: 'Personal Project',
-      year: 2023,
-    },
-  ];
-
-  constructor(
-    private $helperService: HelperService,
-    private viewContainerRef: ViewContainerRef
-  ) {
-    this.$helperService.checkBreakpoint();
-  }
-
-  ngOnInit(): void {
-    console.log('debug', this.viewContainerRef);
+  constructor() {
   }
 }
--- src/app/filename-to-image-alt.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilenameToImageAltPipe } from './filename-to-image-alt.pipe';
-
-describe('FilenameToImageAltPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilenameToImageAltPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
